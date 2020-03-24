66be4cd29c52d691237b10893dce20709f4bf587
jenissabarrera <Jenissa.Barrera@genesys.com>
2020-03-24 10:05:17 +0800

    Added client ids per genesys cloud region.

--- docs/wizard/config/config.js
+++ docs/wizard/config/config.js
@@ -1,9 +1,12 @@
 export default {
     clientIDs: {        
-       'mypurecloud.com': '83d37bf5-e050-47bf-9937-0314b259c9c4'
-//         'mypurecloud.ie': '939ab4dd-109f-4120-ba9f-051b973b9ecc',
-//         'mypurecloud.com.au': 'c8a4d721-3fbb-4f50-b3e0-aa49bf86ac87',
-//         'mypurecloud.jp': '28dbeebd-8128-4fe0-8f42-f2eebb767a71'
+        'mypurecloud.com': 'e4163c90-2168-400a-9441-cb2c8ae7ccc2',
+        'mypurecloud.ie': '377bf436-7787-4ed0-83d7-7ade528ff4ed',
+        'mypurecloud.com.au': 'ae206cd0-a70e-481d-86f4-b1bfb61498de',
+        'mypurecloud.jp': '265993a4-931a-44e5-a0b1-80519c3edb25',
+        'mypurecloud.de':  '5f25a1d0-7b42-416b-beb3-fa708e7b2e65',
+        'usw2.pure.cloud': '0d09a06e-f09a-47ed-8556-080479596414'
+        
     },
     //'wizardUriBase': 'http://localhost:3000/wizard/',
     'wizardUriBase': 'https://jenissabarrera.github.io/devorgProvisioningTool/wizard/',
